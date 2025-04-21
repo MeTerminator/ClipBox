e2c5523a5709a02049f1146af6c138d7624d5175
MeTerminator <42143735+MeTerminator@users.noreply.github.com>
2025-04-21 22:59:44 +0800

    Update uptime.py

--- app/routes/uptime.py
+++ app/routes/uptime.py
@@ -3,9 +3,9 @@ from app.extensions import redis_client
 import json
 import requests
 import time
+import threading
 
 uptime_bp = Blueprint('uptime', __name__)
-
 
 NODES_NAME = {
     "k1": "New York",
@@ -16,13 +16,11 @@ NODES_NAME = {
 
 last_updated = -1
 
-
 def split_list(data):
     last_two = data[-2:]
     remaining = data[:-2]
     groups = [remaining[i:i+3] for i in range(0, len(remaining), 3)]
     return groups, last_two
-
 
 def get_uptime_data():
     # Request uptime data
@@ -85,37 +83,32 @@ def get_uptime_data():
 
     return uptime_data
 
+def update_redis_periodically(interval=5):
+    global last_updated
+    while True:
+        current_time = time.time()
+        if last_updated == -1 or (current_time - last_updated) > interval:
+            last_updated = current_time
+            uptime_data = get_uptime_data()
+            redis_client.set("UPTIME_DATA", json.dumps(uptime_data))
+        time.sleep(interval)
 
 @uptime_bp.route("/details")
 def get_uptime_details():
     global redis_client
-    global last_updated
-    if last_updated == -1 or (time.time() - last_updated) > 60:
-        last_updated = time.time()
-        uptime_data = get_uptime_data()
-        redis_client.set("UPTIME_DATA", json.dumps(uptime_data))
-    else:
-        try:
-            uptime_data = json.loads(redis_client.get("UPTIME_DATA"))
-        except:
-            uptime_data = {}
+    try:
+        uptime_data = json.loads(redis_client.get("UPTIME_DATA"))
+    except:
+        uptime_data = {}
     return jsonify(uptime_data)
-
 
 @uptime_bp.route("/")
 def get_uptime():
     global redis_client
-    global last_updated
-
-    if last_updated == -1 or (time.time() - last_updated) > 60:
-        last_updated = time.time()
-        uptime_data = get_uptime_data()
-        redis_client.set("UPTIME_DATA", json.dumps(uptime_data))
-    else:
-        try:
-            uptime_data = json.loads(redis_client.get("UPTIME_DATA"))
-        except:
-            uptime_data = {}
+    try:
+        uptime_data = json.loads(redis_client.get("UPTIME_DATA"))
+    except:
+        uptime_data = {}
 
     response = make_response(jsonify({
         "status": uptime_data.get("status"),
@@ -125,3 +118,11 @@ def get_uptime():
     response.headers["Uptime-Status"] = str(uptime_data.get("status")).capitalize()
 
     return response
+
+# Start the periodic update thread when the application starts
+thread = threading.Thread(target=update_redis_periodically)
+thread.daemon = True
+thread.start()
+
+
+
