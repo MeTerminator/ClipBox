ca4d100082eea786640fbf23aa1193dd4a29b2e0
MeTerminator <42143735+MeTerminator@users.noreply.github.com>
2025-06-03 13:45:52 +0800

    New Player

--- app/config.py
+++ app/config.py
@@ -1,5 +1,12 @@
 import os
 
 
+def get_redis_url():
+    url = os.getenv("REDIS_URL")
+    if url:
+        url = url.strip('"')
+    return url
+
+
 class Config:
-    REDIS_URL = os.getenv("REDIS_URL")
+    REDIS_URL = get_redis_url()
